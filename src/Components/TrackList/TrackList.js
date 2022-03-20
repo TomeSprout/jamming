2519a02cc488cacb21d9a743126e75080a66c554
Timothy <Rorie.TimothyJr@gmail.com>
2022-03-20 19:51:21 -0400

    Add TrackList Component, map function

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TrackList.css';
+
+import Track from '../Track/Track';
+
+export default class TrackList extends React.Component {
+	render () {
+		return (
+			<div class="TrackList">
+				{this.props.tracks.map((track) => {
+					return <Track track={track} key={track.id} />;
+				})}
+			</div>
+		);
+	}
+}
