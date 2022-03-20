aa0747af332f6c08c27d878d4076be1885862aba
Timothy <Rorie.TimothyJr@gmail.com>
2022-03-20 19:52:33 -0400

    Link dependent component files. Add constructor

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,52 @@
+import { render } from '@testing-library/react';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-export default App;
+export default class App extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			searchResults : [
+				{
+					name   : 'trackName1',
+					artist : 'trackArtist1',
+					album  : 'albumName1',
+					id     : 1,
+				},
+				{
+					name   : 'trackName2',
+					artist : 'trackArtist2',
+					album  : 'albumName2',
+					id     : 2,
+				},
+				{
+					name   : 'trackName3',
+					artist : 'trackArtist3',
+					album  : 'albumName3',
+					id     : 3,
+				},
+			],
+		};
+	}
+
+	render () {
+		return (
+			<div>
+				<h1>
+					Ja<span className="highlight">mmm</span>ing
+				</h1>
+				<div className="App">
+					<SearchBar />
+					<div className="App-playlist">
+						<SearchResults searchResults={this.state.searchResults} />
+						<Playlist />
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
