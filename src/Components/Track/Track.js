dfd872137bcd9c9f0610b2f2f792b4c3549d7b9a
Timothy <Rorie.TimothyJr@gmail.com>
2022-03-23 11:13:22 -0400

    Properly display track details

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,10 +4,10 @@ import './Track.css';
 export default class Track extends React.Component {
 	renderAction () {
 		if (this.props.isRemoval) {
-			return <button className="Track-action">-</button>;
+			return <button className="Track-action"> - </button>;
 		}
 		else {
-			return <button className="Track-action">+</button>;
+			return <button className="Track-action"> + </button>;
 		}
 	}
 
@@ -15,8 +15,10 @@ export default class Track extends React.Component {
 		return (
 			<div className="Track">
 				<div className="Track-information">
-					<h3>trackname</h3>
-					<p>trackartist | trackalbum</p>
+					<h3>{this.props.track.name}</h3>
+					<p>
+						{this.props.track.artist} | {this.props.track.album}
+					</p>
 				</div>
 				{this.renderAction()}
 			</div>
