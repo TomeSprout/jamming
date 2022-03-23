84e7a1d8377d20e1df75d0a448aec63b987c09df
Timothy <Rorie.TimothyJr@gmail.com>
2022-03-23 11:42:41 -0400

    Fix method pass to component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -75,7 +75,7 @@ export default class App extends React.Component {
 				<div className="App">
 					<SearchBar />
 					<div className="App-playlist">
-						<SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack()} />
+						<SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
 						<Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
 					</div>
 				</div>
