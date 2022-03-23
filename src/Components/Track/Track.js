8fdda2f3bc7f051fce63ac2f3e3ad14088a40480
Timothy <Rorie.TimothyJr@gmail.com>
2022-03-23 11:47:07 -0400

    Add constructor. Bind addTrack method

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,16 +3,21 @@ import Playlist from '../Playlist/Playlist';
 import './Track.css';
 
 export default class Track extends React.Component {
+	constructor (props) {
+		super(props);
+		this.addTrack = this.addTrack.bind(this);
+	}
+
 	renderAction () {
 		if (this.props.isRemoval) {
 			return <button className="Track-action"> - </button>;
 		}
 		else {
-			return <button className="Track-action"> + </button>;
+			return <button className="Track-action" onClick={this.addTrack} > + </button>;
 		}
 	}
 
-	addTrack() {
+	addTrack () {
 		this.props.onAdd(this.props.track);
 	}
 
